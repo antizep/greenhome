faacbeca4fd50dd4430b038d4c0c523c655f70c2
antizep <antizep@mail.ru>
2026-04-22 21:27:56 +0300

    Шлюз для команд в ардуину.

--- src/main/java/ru/antizep/greenhouse/Greenhouse.java
+++ src/main/java/ru/antizep/greenhouse/Greenhouse.java
@@ -6,8 +6,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling // Понадобится для опроса Arduino по расписанию
-public class Greenhouse {
-
+public class Greenhouse { 
 	public static void main(String[] args) {
 		SpringApplication.run(Greenhouse.class, args);
 	}
